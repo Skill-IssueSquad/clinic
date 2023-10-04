0d9d5b6575a846a359206500a295a3d14bc424db
Omar Nour <omar.syd.n@gmail.com>
2023-10-04 21:12:32 +0300

    add base code

---
+++ backend/backend.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).then(() => {
+    app.listen(8000, () => console.log('Server Started'))
+}).catch(err => console.log(err))
+
+
+app.use(express.json())
+
+//const subscribersRouter = require('./routes/subscribers')
+//app.use('/subscribers', subscribersRouter)
