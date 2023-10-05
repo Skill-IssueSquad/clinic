098f3c49157e1333dd67b7bc759bf75faa558a75
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-10-05 19:00:42 +0300

    applied formatter

--- backend/backend.js
+++ backend/backend.js
@@ -1,15 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).then(() => {
-    app.listen(8000, () => console.log('Server Started'))
-}).catch(err => console.log(err))
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(8000, () => console.log("Server Started"));
+  })
+  .catch((err) => console.log(err));
 
-
-app.use(express.json())
+app.use(express.json());
 
 //const subscribersRouter = require('./routes/subscribers')
 //app.use('/subscribers', subscribersRouter)
