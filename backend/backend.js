a6afefa7c5e347e53d354fabfbb36d04ea027480
Omar Nour <52005044+Omar-Nour@users.noreply.github.com>
2023-10-06 17:08:43 +0300

    Fix router path for Doctor

--- backend/backend.js
+++ backend/backend.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const doctorRouter = require("./routes/DoctorRouter");
+const doctorRouter = require("./src/routes/DoctorRouter");
 
 mongoose
   .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
