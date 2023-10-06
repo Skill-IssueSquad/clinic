91f9e39bf3d540d5079bbd8c32c8dfe9f2c34d2a
Mohamed Shamekh <shamekhjr@gmail.com>
2023-10-06 21:42:01 +0300

    Implemented add Family Member in backend

--- backend/src/controllers/PatientController.js
+++ backend/src/controllers/PatientController.js
@@ -5,9 +5,10 @@ const mongoose = require("mongoose");
 //be filling the registration form and we would be creating a new patient)
 const addFamMember = async (req, res) => {
   const { username } = req.params;
-  const { name, nationalID, age, gender, relation } = req.body;
+  const { name, nationalID, age, gender, relation, healthPackageType } =
+    req.body;
 
-  const patient = await Patient.find({ username: username }).catch((err) => {
+  const patient = await Patient.findOne({ username: username }).catch((err) => {
     return res.status(500).json({
       success: false,
       data: null,
@@ -22,10 +23,49 @@ const addFamMember = async (req, res) => {
       message: "Patient not found",
     });
   }
-  console.log(patient.extfamilyMembers);
-  return res.status(200).json({
+
+  //check if extFamilyMembers array is undefined
+  if (!patient.extfamilyMembers) {
+    patient.extfamilyMembers = [];
+  }
+
+  //new family member info
+  const newFamMember = {
+    name: name,
+    national_id: nationalID,
+    relation: relation, //wife, husband, son, daughter
+    age: age,
+    gender: gender, //M, F, Bahy
+    healthPackageType: healthPackageType,
+  };
+
+  //add to extfamilyMembers array
+  patient.extfamilyMembers.push(newFamMember);
+
+  //update the existing patient
+  await Patient.findByIdAndUpdate(patient._id, {
+    extfamilyMembers: patient.extfamilyMembers,
+  }).catch((err) => {
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "${err.message}",
+    });
+  });
+
+  const newPatient = await Patient.findOne({ username: username }).catch(
+    (err) => {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: "${err.message}",
+      });
+    }
+  );
+
+  res.status(200).json({
     success: true,
-    data: patient.extfamilyMembers,
+    data: newPatient,
     message: "Family member added successfully",
   });
 };
