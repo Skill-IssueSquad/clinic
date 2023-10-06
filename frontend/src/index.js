e1584137c6a32dac5db9e4cb806d71fbdcc43bf2
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-10-07 00:22:21 +0300

    syntax error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Doctor from "./pages/Doctor";
 import DoctorProfile from "./pages/DoctorProfile";
 import Patients from "./pages/Patients";
-import MultiFilter from "./components/MultiFilter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<MultiFilter />} />
         <Route exact path="/Doctor_Home" element={<Doctor />} />
         <Route exact path="/Doctor_Profile" element={<DoctorProfile />} />
         <Route exact path="/Doctor_Patients" element={<Patients />} />
