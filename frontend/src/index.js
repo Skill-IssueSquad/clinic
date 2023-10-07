3ddeb39c0dbdb802903e2b160721d24da38c729e
Mohamed Shamekh <shamekhjr@gmail.com>
2023-10-07 15:47:07 +0300

    Added Patient page for testing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AppBar  from './components/appBar';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppBar from "./components/appBar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppBar hh="" gklh=""/>
+    <AppBar hh="" gklh="" />
     <App />
   </React.StrictMode>
 );
---
+++ frontend/src/pages/Patient.js
@@ -0,0 +1,9 @@
+const Patient = () => {
+  return (
+    <div className="patient">
+      <h2>Patient</h2>
+    </div>
+  );
+};
+
+export default Patient;
