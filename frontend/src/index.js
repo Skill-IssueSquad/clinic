5f72faaa6f1c695104081a514bf1ad53708f774b
Omar Nour <omar.syd.n@gmail.com>
2023-10-15 04:33:51 +0300

    fix merge issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import AppBar from "./components/appBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <AppBar hh="" gklh="" />
