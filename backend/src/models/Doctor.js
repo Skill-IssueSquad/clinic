67b79f23a6d25e5f2950363b17c15ebfb205db4c
Omar Nour <omar.syd.n@gmail.com>
2023-10-15 05:19:30 +0300

    Fix schema duplication

--- backend/src/models/Doctor.js
+++ backend/src/models/Doctor.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 const doctorSchema = new Schema({
   username: {
