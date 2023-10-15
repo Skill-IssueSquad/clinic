27ca771d4328c91e5de4f05f47ce7c8dbbeb2851
Omar Nour <omar.syd.n@gmail.com>
2023-10-15 05:18:59 +0300

    after a days work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./App";
 import AppBar from "./components/appBar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    
     <App />
   </React.StrictMode>
 );
