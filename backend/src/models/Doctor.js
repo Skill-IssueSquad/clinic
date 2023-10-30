daa88fd2eadcda3e08e1af1e0cbd9be8f18269f2
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-10-30 21:37:27 +0200

    Add wallet to Doctor model

--- backend/src/models/Doctor.js
+++ backend/src/models/Doctor.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const doctorSchema = new Schema({
   username: {
     type: String,
@@ -76,6 +75,10 @@ const doctorSchema = new Schema({
     ],
     required: true,
     default: [],
+  },
+  walletBalance: {
+    type: Number,
+    default: 0,
   },
 });
 
