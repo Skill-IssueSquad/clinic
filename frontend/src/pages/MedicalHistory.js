ae539d901ffcac3f524fb50f5ffd99abdbc390a8
youssefalaa398 <119411027+youssefalaa398@users.noreply.github.com>
2023-11-10 20:23:33 +0200

    implemented in frontend but has errors

--- frontend/src/pages/MedicalHistory.js
+++ frontend/src/pages/MedicalHistory.js
@@ -3,42 +3,59 @@ import axios from "axios";
 
 const MedicalHistory = () => {
   const [email, setEmail] = useState("");
-  const [File, setFile] = useState("");
+  const [file, setFile] = useState(null);
 
- 
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
+  const handleUpload = async () => {
+    if (!file) {
+      alert("Please select a file.");
+      return;
+    }
 
+    const formData = new FormData();
+    formData.append("document", file);
 
-    useEffect(() => {
-    const fetchPatient = async () => {
-      await axios.get("http://localhost:8000/patient/p8two").then((res) => {
-        setEmail(res.data.data.email);
+    try {
+      await axios.post(`http://localhost:8000/patients/p8two/healthrecords`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       });
+
+      // Optionally, you can do something after successful upload
+      alert("File uploaded successfully!");
+    } catch (error) {
+      // Handle error
+      console.error("Error uploading file:", error.message);
+      alert("File upload failed. Please try again.");
+    }
+  };
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/patient/p8two");
+        setEmail(response.data.data.email);
+      } catch (error) {
+        console.error("Error fetching patient:", error.message);
+      }
     };
 
-    
     fetchPatient();
-    /*const formData = new FormData();
-    formData.append('File', File);
-    axios.post('http://localhost:8000/patients/p8two/healthrecords', formData )*/
-
-
-
-  }, []); // The empty dependency array ensures that this effect runs once when the component mounts
+  }, []);
 
   return (
     <div>
       <h1>Medical History</h1>
-    
-      {email && (
-        <p>
-          Patient's Email: {email}
-        </p>
-      )}
 
-      <input type="file" onChange={(e)=>setFile(e.target.files[0])}/>
-        
+      {email && <p>Patient's Email: {email}</p>}
 
+      <input type="file" onChange={handleFileChange} />
+
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
 };
