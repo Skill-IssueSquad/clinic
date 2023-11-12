a53e9ee7d58571d639ecafbc0dc85e96c56568dd
Omar Nour <omar.syd.n@gmail.com>
2023-11-12 20:03:15 +0200

    wallet balance displayed component

--- frontend/src/components/Patient/WalletBalance.js
+++ frontend/src/components/Patient/WalletBalance.js
@@ -1,48 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Loading from "../Loading";
-import { Grid, Button, TextField } from "@mui/material";
 import axios from "axios";
 
 
-const WalletBalance = () => {
-    const [balance, setBalance] = useState(0);
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        axios
-        .get("http://localhost:3000/api/patient/walletBalance", {
-            headers: {
-            "auth-token": localStorage.getItem("token"),
-            },
-        })
-        .then((res) => {
-            setBalance(res.data.balance);
-            setLoading(false);
-        })
-        .catch((err) => {
-            console.log(err);
-            setLoading(false);
-        });
-    }, []);
-    
-    return (
-        <div className="WalletBalance">
-        {loading ? (
-            <Loading />
-        ) : (
-            <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <TextField
-                label="Wallet Balance"
-                value={balance}
-                disabled
-                fullWidth
-                />
-            </Grid>
-            </Grid>
-        )}
+//get patient detials
+const patientDetails = async (username) => {
+  const res = await axios.get(`/patient/getPatient/${username}`);
+  return res.data;
+};
+
+//display the wallet balance of the patient
+const WalletBalance = ({ username }) => {
+  const [patient, setPatient] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getPatient = async () => {
+      const patientFromServer = await patientDetails(username);
+      setPatient(patientFromServer);
+      setLoading(false);
+    };
+    getPatient();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <h1>Wallet Balance</h1>
+          <h2>{patient.walletBalance}</h2>
         </div>
-    );
-    }
+      )}
+    </div>
+  );
+};
+
 
 export default WalletBalance;
--- frontend/src/pages/Patient/WalletBalance.js
+++ frontend/src/pages/Patient/WalletBalance.js
@@ -1,10 +1,11 @@
 import AppBar from "../../components/appBar";
+import WalletBalance from "../../components/Patient/WalletBalance";
 
 function WalletBalance() {
   return (
     <div className="WalletBalance">
       <AppBar/>
-      
+      <WalletBalance/>
     </div>
   );
 }
