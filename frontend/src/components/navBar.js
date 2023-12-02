44b7f8ca73c8df526a23d69d68a6a773ada42a85
roshdyahmed2002 <roshdy.ahmed26@yahoo.com>
2023-12-02 03:56:14 +0200

    red dot on bell in all except home

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,8 +13,8 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import AdbIcon from '@mui/icons-material/Adb';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const pages = ['Home', 'Doctors', 'Appointments', 'Medical History', 'Health Packages'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -22,10 +23,30 @@ const ResponsiveAppBar = ({ username }) => {
   let navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [hasUnseenNotifications, setHasUnseenNotifications] = React.useState(false);
+
+  useEffect(() => {
+    const fetchUnseenNotifications = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/patient/getAllUnseenNotifications/${username}`
+        );
+
+        const unseenNotifications = response.data.data;
+        const hasUnseen = unseenNotifications.some(notification => !notification.isSeen);
+        setHasUnseenNotifications(hasUnseen);
+      } catch (error) {
+        console.error("Error fetching unseen notifications:", error.message);
+      }
+    };
+
+    fetchUnseenNotifications();
+  }, [username]);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -37,6 +58,7 @@ const ResponsiveAppBar = ({ username }) => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
   const handleBellIconClick = () => {
     // Navigate to the notifications page or any other desired page
     navigate('/patient/notifications');
@@ -139,6 +161,19 @@ const ResponsiveAppBar = ({ username }) => {
                 onClick={handleBellIconClick} 
               >
                 <NotificationsIcon />
+                {hasUnseenNotifications && (
+                  <span
+                    style={{
+                      position: 'absolute',
+                      top: '8px',
+                      right: '8px',
+                      background: 'red',
+                      borderRadius: '50%',
+                      width: '10px',
+                      height: '10px',
+                    }}
+                  />
+                )}
               </IconButton>
             </Tooltip>
           </Box>
