5096bc9eca5668d79239310285a0a328a232212d
shamekhjr <shamekhjr@gmail.com>
2023-12-05 00:48:23 +0200

    video feed is now displayed but it displays the same feed twice

--- frontend/src/components/Patient/VideoCallGrid.js
+++ frontend/src/components/Patient/VideoCallGrid.js
@@ -4,74 +4,113 @@ import io from "socket.io-client";
 import Peer from "peerjs";
 
 const VideoCall = () => {
-  const gridContainerRef = useRef(null);
+  const videoGridRef = useRef(null);
+  let myPeer;
+  let peers = {};
 
   useEffect(() => {
-    // Your socket and peerJS logic here
     const roomId = window.location.pathname.split("/")[2];
     const socket = io("http://localhost:8001");
-    const myPeer = new Peer(undefined, {
+    myPeer = new Peer(undefined, {
       host: "/",
       port: "3001",
+    });
+
+    const myVideo = document.createElement("video");
+    myVideo.muted = true;
+
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        addVideoStream(myVideo, stream);
+
+        //when we recieve a call we want to answer it and send our stream to the caller
+        myPeer.on("call", (call) => {
+          call.answer(stream);
+          const video = document.createElement("video");
+
+          //respond to the video stream
+          call.on("stream", (userVideoStream) => {
+            addVideoStream(video, userVideoStream);
+          });
+
+          //if a user leaves the room we want to remove their video
+          call.on("close", () => {
+            video.remove();
+          });
+        });
+
+        //if a new user joins the room we want to send our stream to that user
+        socket.on("user-connected", (userId) => {
+          connectToNewUser(userId, stream);
+        });
+      });
+
+    //when a user disconnects we want to remove their video
+    socket.on("user-disconnected", (userId) => {
+      if (peers[userId]) peers[userId].close();
     });
 
     myPeer.on("open", (id) => {
       console.log("peer id", id);
       socket.emit("join-room", roomId, id);
     });
-
-    socket.on("user-connected", (userId) => {
-      console.log("user connected", userId);
-      addVideoFeed(userId, "nagnoog", "Patient");
-    });
-
-    // Dummy function to simulate adding video feeds
-    const addVideoFeed = (userId, name, role) => {
-      const newCard = document.createElement("div");
-      newCard.innerHTML = `
-        <div style="width: 100%; position: relative; padding-top: 56.25%;">
-          <video
-            autoplay
-            muted
-            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-          ></video>
-          <div style="position: absolute; bottom: 0; left: 0; right: 0; background: rgba(255, 255, 255, 0.8); padding: 8px;">
-            <div>${name}</div>
-            <div>${role}</div>
-          </div>
-        </div>
-      `;
-
-      const gridContainer = gridContainerRef.current;
-      if (gridContainer) {
-        gridContainer.appendChild(newCard);
-      }
-
-      // Your logic to handle peerJS call and video stream here
-      // Example: const call = peer.call(userId, stream);
-      // call.on('stream', (remoteStream) => {
-      //   const videoElement = newCard.querySelector('video');
-      //   videoElement.srcObject = remoteStream;
-      // });
-    };
-
-    // Example: Call addVideoFeed when someone joins the room
-    // addVideoFeed('userId1', 'John Doe', 'Doctor');
-
-    // Cleanup logic when the component unmounts
-    return () => {
-      // Your cleanup logic here
-    };
   }, []);
 
+  const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+      video.play();
+    });
+
+    if (videoGridRef.current) {
+      videoGridRef.current.appendChild(video);
+    }
+  };
+
+  const connectToNewUser = (userId, stream) => {
+    //call user with said userId and send our stream to that user
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement("video");
+
+    //when we recieve a stream from the user we called we want to add it to our video grid
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+
+    //if a user leaves the room we want to remove their video
+    call.on("close", () => {
+      video.remove();
+    });
+
+    peers[userId] = call;
+  };
+
+  // Dummy data, replace with actual user data
+  const users = [
+    { id: 1, name: "John Doe", role: "Doctor" },
+    { id: 2, name: "Jane Smith", role: "Patient" },
+    // Add more users as needed
+  ];
+
   return (
-    <Grid
-      container
-      spacing={2}
-      ref={gridContainerRef}
-      style={{ height: "100vh", padding: "20px" }}
-    >
-      {/* Video feeds dynamically added here */}
+    <Grid container spacing={2} ref={videoGridRef}>
+      <video autoPlay playsInline muted />
+      {users.map((user) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={user.id}>
+          {/* <Card>
+            <CardContent>
+              <Typography variant="h6">{user.name}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {user.role}
+              </Typography> 
+            </CardContent>
+          </Card> */}
+        </Grid>
+      ))}
     </Grid>
   );
 };
--- frontend/src/pages/CallDoctor.js
+++ frontend/src/pages/CallDoctor.js
@@ -1,11 +1,14 @@
+// VideoCallPage.js
 import React from "react";
+import ResponsiveAppBar from "../components/navBar";
 import VideoCall from "../components/Patient/VideoCallGrid";
 
 const CallDoctor = () => {
   return (
     <div>
+      <ResponsiveAppBar />
       <h1>Call Doctor</h1>
-      <VideoCall> </VideoCall>
+      <VideoCall />
     </div>
   );
 };
